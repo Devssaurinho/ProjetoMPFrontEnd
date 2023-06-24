5cbcb5fd7a97fb9f518996635463c6cbbdbd7c48
Devssaurinho <andressa49araujo@gmail.com>
2023-06-24 02:13:01 -0300

    Ajustes tela de configurações para o administrador

--- src/modules/admin/painel.jsx
+++ src/modules/admin/painel.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TagsInput } from 'react-tag-input-component';
 
 export default function Painel() {
+  const [selected, setSelected] = useState(['Waifus', 'Jogos', 'Livros']);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-800">
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <form className="m-10">
             <div className="space-y-12">
@@ -19,52 +22,31 @@ export default function Painel() {
               <div className="border-b border-gray-900/10 pb-12">
                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                   <div className="sm:col-span-4">
-                    <label htmlFor="preference" className="block text-sm font-medium leading-6 text-white">
-                      Adicione ou remova preferências
-                    </label>
                     <div className="mt-2 flex items-center">
-                      <input
-                        id="preference"
-                        name="preference"
-                        type="text"
-                        autoComplete="off"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Digite a preferência"
-                      />
+                      <div>
+                        <h1 className="py-3 block text-sm font-medium leading-6 text-white">Adicione ou remova preferências</h1>
+
+                        {/* <pre>{JSON.stringify(selected)}</pre> */}
+
+                        <TagsInput
+                          value={selected}
+                          onChange={setSelected}
+                          name="preferencias"
+                          placeHolder="Digite"
+                        />
+                        <em className="text-white">Pressione o enter para adicionar a nova preferência</em>
+                      </div>
                       <div className="px-3 flex gap-x-2">
                         <button
                           type="button"
                           className="px-3 py-1.5 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                          Adicionar
-                        </button>
-                        <button
-                          type="button"
-                          className="px-3 py-1.5 text-sm font-semibold text-white bg-red-600 rounded-md shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                        >
-                          Remover
+                          Salvar
                         </button>
                       </div>
                     </div>
                   </div>
 
-                  <div className="sm:col-span-3 ">
-                    <label htmlFor="preferences" className="block text-sm font-medium leading-6 text-white">
-                      Selecione as preferências que ficarão disponíveis para os usuários
-                    </label>
-                    <div className="mt-2">
-                      <select
-                        id="preferences"
-                        name="preferences"
-                        autoComplete="preferences-name"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                      >
-                        <option>Livros</option>
-                        <option>Jogos</option>
-                        <option>Waifus</option>
-                      </select>
-                    </div>
-                  </div>
                 </div>
                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                   <div className="col-span-full">
@@ -85,15 +67,13 @@ export default function Painel() {
             </div>
 
             <div className="mt-6 flex items-center justify-end gap-x-6">
-              <a href="/" type="link" className="text-sm font-semibold leading-6 text-white">
-                Voltar
-              </a>
-              <button
-                type="submit"
+              <a
+                type="link"
+                href="/"
                 className="rounded-md bg-[#4e42ac] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Salvar
-              </button>
+                Voltar
+              </a>
             </div>
           </form>
         </div>

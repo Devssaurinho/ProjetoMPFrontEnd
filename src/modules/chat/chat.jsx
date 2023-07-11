ffbf60ca450fe87fe9877e02692973ae313fcc70
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2023-07-11 19:41:58 -0300

    chat privado dos infernos pronto!!

--- src/modules/chat/chat.jsx
+++ src/modules/chat/chat.jsx
@@ -1,17 +1,121 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Chat() {
+function Chat() {
+  const [amigos, setAmigos] = useState([]);
+  const [chatAberto, setChatAberto] = useState(false);
+  const [mensagens, setMensagens] = useState([]);
+  const userData = JSON.parse(localStorage.getItem('responseData'));
+  const [msg, setInputValue] = useState('');
+  const [amigoSelecionado, setAmigoSelecionado] = useState('');
+
+  const handleChange = (event) => { setInputValue(event.target.value); }; // Atualiza o valor do input conforme o usuário digita
+
+  const getFriendName = async (friendId) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/Usuarios/lista-usuario-por-id/${friendId}`);
+      const friend = response.data;
+      return friend.username; // Use a propriedade correta que contém o nome do amigo
+    } catch (error) {
+      console.log(`Erro ao buscar o amigo com ID ${friendId}: ${error}`);
+      return '';
+    }
+  };
+
+  const getUserIdByUsername = async (username) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/Usuarios/lista-usuario-por-username/${username}`);
+      const user = response.data;
+      return user.id; // Use a propriedade correta que contém o ID do usuário
+    } catch (error) {
+      console.log(`Erro ao buscar o ID do usuário com username ${username}: ${error}`);
+      return '';
+    }
+  };
+
+  const handleFriendClick = async (username) => {
+    console.log(username);
+    setAmigoSelecionado(username);
+    try {
+      const friendId = await getUserIdByUsername(username);
+      // Buscar mensagens
+      const response = await axios.get(`http://localhost:8000/ChatPrivado/buscar-mensagens/${userData.id}/${friendId}`);
+      const resposta = response.data; // Supondo que a resposta seja um array de mensagens
+      console.log(resposta);
+      setMensagens(resposta);
+      // Abrir o chat
+      console.log(setMensagens);
+      setChatAberto(true);
+      console.log(chatAberto);
+    } catch (error) {
+      console.log(`Erro ao abrir o chat com o amigo ${username}: ${error}`);
+    }
+  };
+
+  const enviarMensagem = async (mensagem) => {
+    try {
+      console.log(`Mensagem para ${amigoSelecionado}: ${mensagem}`);
+      const novoAmigo = await getUserIdByUsername(amigoSelecionado);
+      console.log(novoAmigo);
+      // Enviar mensagem
+      await axios.post(`http://localhost:8000/ChatPrivado/enviar-mensagem/${userData.id}/${novoAmigo}`, {
+        mensagem,
+      });
+      // Atualizar as mensagens exibidas
+      // Você pode fazer uma nova chamada à API para buscar as mensagens atualizadas ou atualizar o estado localmente
+    } catch (error) {
+      console.log(`Erro ao enviar mensagem: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    const fetchAmigos = async () => {
+      if (userData && userData.amigos) {
+        const friendsArray = userData.amigos;
+        const formattedFriends = await Promise.all(friendsArray.map((friendId) => getFriendName(friendId)));
+        setAmigos(formattedFriends);
+      }
+    };
+
+    fetchAmigos();
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-800">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-[#4e43ac] px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-          <div className="container py-20 content">
-            <h2 className="mb-4 text-white text-2xl font-bold">
-              Chat
-            </h2>
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+          <div className="relative mt-16 h-auto lg:mt-1 py-5">
+            <div className="mb-4 text-white text-2xl font-bold">
+              <h2>Lista de Amigos:</h2>
+              <ul>
+                {amigos.map((amigo) => (
+                  <li key={amigo}>
+                    <button className="text-white" type="button" onClick={() => handleFriendClick(amigo)}>
+                      {amigo}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
+          {chatAberto && (
+            <div className="relative mt-16 h-auto lg:mt-1 py-5">
+              <div>
+                <h2>Chat:</h2>
+                <ul>
+                  {mensagens.map((mensagem) => (
+                    <li key={mensagem.id}>{mensagem.mensagem}</li>
+                  ))}
+                </ul>
+                <input type="text" placeholder="Digite sua mensagem" value={msg} onChange={handleChange} />
+                <button type="button" onClick={() => enviarMensagem(msg)}>Enviar</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 }
+
+export default Chat;
